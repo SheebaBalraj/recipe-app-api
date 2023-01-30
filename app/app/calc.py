3bacee3037cdef44d2e2c3dccf43d6e678d7b5f9
Sheeba Balraj <sheeba9845@gmail.com>
2023-01-30 12:55:39 +0530

    Added Postgres

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,11 @@
 Calculator Functions
 """
 
-def add(x,y):
+def add(x, y):
     """Add x and y and return result"""
     return x+y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract x and y and return result"""
     return y-x
